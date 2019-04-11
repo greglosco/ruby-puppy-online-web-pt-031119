7f0f0326c07f7a9053b4654752b11fb74b08ae21
Gregory Losco <gtlosco@gmail.com>
2019-04-11 21:16:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,12 +9,12 @@ class Dog
     @@all << self
   end
   
-  def self.clear_all
-    @@all.clear
-  end
-  
   def self.all
     @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
   end
   
 end 
